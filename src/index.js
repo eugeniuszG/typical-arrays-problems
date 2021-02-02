379a1ac5e8271659cf4986702e09ea9a36cac77b
Eugeniusz Gronski <41344500+eugeniuszG@users.noreply.github.com>
2021-02-02 17:34:50 +0100

    feat: add solutions for min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0 
+  } 
+  if (array.length) {
+    let min = array[0]
+    array.forEach(element => {
+      if (element < min) {
+        min = element;
+      }
+    });
+    return min;  
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0 
+  }  
+  if (array.length) {
+    let max = array[0]
+    array.forEach(element => {
+      if (element > max) {
+        max = element;
+      }
+    });  
+    return max
+  }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0 
+  } 
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return (array.reduce(reducer))/array.length;
 }
